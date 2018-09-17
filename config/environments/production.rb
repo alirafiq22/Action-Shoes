c0a4d140af53aec813a5df79863256710240e8c9
alirafiq22 <alirafiq_01@yahoo.com>
2018-09-17 22:11:32 +0530

    update prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ ActionShoes::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-#config.assets.initialize_on_precompile = false
+config.assets.initialize_on_precompile = false
 #config.assets.js_compressor = Uglifier.new(harmony: true)
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
